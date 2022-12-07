6e57591511c0f3b88365e1aec5a9badcb7c8e704
Edson MZ <emz19.com@gmail.com>
2022-12-06 19:18:11 -0600

    added ordering dependecy injection

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using Ordering.Application;
+using Ordering.Infrastructure;
 
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
+IWebHostEnvironment environment = builder.Environment;
 // Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureService(configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
