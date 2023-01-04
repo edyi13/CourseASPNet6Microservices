4670aa92f35d94bd032d68b9e7f10952648a9e88
Edson MZ <emz19.com@gmail.com>
2023-01-03 19:22:50 -0600

    Added rabbitmq

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            Id= Guid.NewGuid();
+            creationDate= DateTime.Now;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime creationDate)
+        {
+            Id = id;
+            this.creationDate = creationDate;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime creationDate { get; set; }
+    }
+}
